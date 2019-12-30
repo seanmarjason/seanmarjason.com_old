71415bef2a71961ecb311a31749d1d3b39852fe9
Sean Marjason <smarj1@hotmail.com>
2019-12-30 23:22:45 +1000

    Add auto-rotate functionality to carousel

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// Menu Show-Hide
+// MENU SHOW-HIDE
 function toggleMenu() {
   var menu = document.getElementById("burgerMenu")
   var hamburger = document.getElementById("burger");
@@ -7,36 +7,83 @@ function toggleMenu() {
   hamburger.classList.toggle("change");
 }
 
-// Carousel
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// CAROUSEL
+// Identify all slides
+var slides = document.getElementsByClassName("slide");
+var dots = document.getElementsByClassName("dot");
+var state = "play"
+var slideIndex = 0;
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
+// On load, Show only the first slide + On press of dots, show current slide
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("skill");
-  var dots = document.getElementsByClassName("dot");
-  
-  if (n > slides.length) {
-    slideIndex = 1
-  };
-
-  if (n < 1) {
-    slideIndex = slides.length
-  };
 
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display="none";
-    dots[i].className = dots[i].className.replace(" active", "");
+    slides[i].classList.replace("active", "inactive");
+    dots[i].classList.remove("activeDot");
   }
 
-  slides[slideIndex-1].style.display = "flex";
-  dots[slideIndex-1].className += " active";
+  if (n > (slides.length - 1)) {
+    slideIndex = 0;
+  }
+
+  if (n < 0) {
+    slideIndex = (slides.length - 1);
+  }
+
+  slides[slideIndex].classList.replace("inactive", "active");
+  slides[slideIndex].classList.add("active");
+  dots[slideIndex].classList.add("activeDot")
 }
+
+window.addEventListener('load', function () {showSlides(slideIndex)});
+
+
+// Automatically rotate through slides 
+var playButton = document.getElementById("play");
+var pauseButton = document.getElementById("pause");
+
+function playSlides() {
+  t  = setInterval(function () {changeSlide(1)}, 5000);
+  pauseButton.classList.replace("hide", "show");
+  playButton.classList.replace("show", "hide");
+}
+
+function pauseSlides() {
+  clearInterval(t);
+  playButton.classList.replace("hide", "show");
+  pauseButton.classList.replace("show", "hide");
+}
+
+document.addEventListener('load', playSlides());
+
+// Play-pause carousel rotation
+var play = document.getElementById("playPause");
+play.addEventListener('click', playPauseSlides);
+
+function playPauseSlides() {
+  if (state === "play") {
+    state = "pause";
+    pauseSlides();
+  }
+
+  else {
+    state = "play";
+    playSlides();
+  }
+}
+
+// On button press, iterate slide index to show next slide / previous slide
+function changeSlide(n) {
+  slideIndex += n;
+  showSlides(slideIndex);
+}
+
+previous = document.getElementById("previous");
+previous.addEventListener('click', function () {changeSlide(-1)});
+previous.addEventListener('click', function () {pauseSlides(t)});
+
+next = document.getElementById("next");
+next.addEventListener('click', function () {changeSlide(1)});
+next.addEventListener('click', function () {pauseSlides(t)});
